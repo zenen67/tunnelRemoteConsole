24f4dda290edf434fd0b118dcffd9fdeeed1b3b9
mff245 <e.deliverycdn.tid@telefonica.com>
2017-08-20 10:04:04 +0200

    Add manufacturers

--- bin/tunnelRemoteConsole.sh
+++ bin/tunnelRemoteConsole.sh
@@ -17,18 +17,19 @@ Basic:
   --help         This message
 
 Advanced parameters:
---hwid [Mandatory] with that options 'hp' 'lenovo' 'huawei'
+--hwid [Mandatory] with these options 'hp' 'lenovo' 'huawei'
 --browser [Optional]  Open a browser configure in the file tunnel.conf
 --ip_gateway_SSH [Mandatory] Linux/Unix server with access to ports of terminal remote.  
 --port_gateway_SSH [Optional]Port of SSH gateway, 
  if not set will be use default port for SSH(22)
 --remoteIP IP remote thats the IP of ILO, for Ex in HP servers case..
 --localIP[Optional] Set a temporal IP in loopback interface, if not set
-the IP used will be 127.0.0.1
+the IP used will be 127.0.0.1, this options allow open multiples remotes consoles.
 
 Ex:
    $0 --help
-   $0
+   $0 --browser --ip_gateway_SSH 213.4.28.160 --port_gateway_SSH 5000 --remoteIP 192.168.0.6\
+   --hwid hp --localIP 1.1.1.1
 "
 }
 # Extract command lines parameters and test it
@@ -93,13 +94,25 @@ fi
 function createtunnel(){
   case $hwid in
          hp)
-           sudo ssh -p $port_gateway_SSH -L $localIP:22:$IP_gateway_SSH:22 \
-             -L $localIP:23:$IP_gateway_SSH:23 -L $localIP:80:$IP_gateway_SSH:80\
-             -L $localIP:443:$IP_gateway_SSH:443  -L $localIP:3389:$IP_gateway_SSH:3389\
-             -L $localIP:17988:$IP_gateway_SSH:17988  -L $localIP:9300:$IP_gateway_SSH:9300\
-             -L $localIP:17990:$IP_gateway_SSH:17990  -L $localIP:3002:$IP_gateway_SSH:3002 \ 
-             $user@$IP_GW
+           sudo ssh -p $port_gateway_SSH -L $localIP:22:$remoteIP:22 \
+             -L $localIP:23:$remoteIP:23 -L $localIP:80:$remoteIP:80\
+             -L $localIP:443:$remoteIP:443  -L $localIP:3389:$remoteIP:3389\
+             -L $localIP:17988:$remoteIP:17988  -L $localIP:9300:$remoteIP:9300\
+             -L $localIP:17990:$remoteIP:17990  -L $localIP:3002:$remoteIP:3002 \ 
+             $username@$ip_gateway_SSH
              ;;
+         huawei)
+           sudo ssh -p $port_gateway_SSH -L $localIP:22:$remoteIP:22 \
+             -L $localIP:23:$remoteIP:23 -L $localIP:80:$remoteIP:80 \
+             -L $localIP:443:$remoteIP:443  -L $localIP:2198:$remoteIP:2198 \
+             -L $localIP:2199:$remoteIP:2199 -L $localIP:8208:$remoteIP:8208 \
+             $username@$ip_gateway_SSH
+             ;;
+         lenovo)
+           sudo ssh -p $port_gateway_SSH -L $localIP:22:$remoteIP:22 \
+             -L $localIP:23:$remoteIP:23 -L $localIP:80:$remoteIP:80\
+             -L $localIP:443:$remoteIP:443  -L $localIP:3900:$remoteIP:3900\
+             $username@$ip_gateway_SSH
   esac           
 }
 # MAIN
